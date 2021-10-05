e36282f3e7bdde708d32fada3d7ffcf6579f3573
Jinwon Seo <jwseo@live.co.kr>
2021-10-05 14:24:05 +0900

    Init project

---
+++ docker-compose-config-all.bat
@@ -0,0 +1,5 @@
+docker-compose ^
+    -f ./sproutfx-platform/docker-compose.common.infra.yml ^
+    -f ./sproutfx-platform/docker-compose.common.services.yml ^
+    -f ./sproutfx-platform/docker-compose.services.sproutfx-platform.yml ^
+    config
